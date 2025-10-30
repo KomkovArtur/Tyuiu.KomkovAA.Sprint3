3cacc9828fffa58ba319666f8d05b1a18173c516
артур комков <stenplay33@gmail.com>
2025-10-30 21:14:17 +0500

    Task4

---
+++ Tyuiu.KomkovAA.Sprint3.Task4.V23.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.KomkovAA.Sprint3.Task4.V23;
+using Tyuiu.KomkovAA.Sprint3.Task4.V23.Lib;
+namespace Tyuiu.KomkovAA.Sprint3.Task4.V23.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int startValue = -5;
+            int stopValue = 5;
+            double res = ds.Calculate(startValue, stopValue);
+            Assert.AreEqual(244.656, res);
+        }
+    }
+}
---
+++ Tyuiu.KomkovAA.Sprint3.Task4.V23/Program.cs
@@ -0,0 +1,33 @@
+﻿using Tyuiu.KomkovAA.Sprint3.Task4.V23;
+using Tyuiu.KomkovAA.Sprint3.Task4.V23.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #3 | Выполнил: Комков А. А. | РППб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Тема: Оператор цикла for                                                *");
+        Console.WriteLine("* Задание #2                                                              *");
+        Console.WriteLine("* Вариант #27                                                             *");
+        Console.WriteLine("* Выполнил: Комков Артур Александрович | РППб-25-1                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* На отрезке, где x принимает значения от -5 до 5, вычислить значение     *");
+        Console.WriteLine("* функции y=(cos(x)/x)+3. При х = 0 прервать цикл.                        *");
+        Console.WriteLine("* Полученные значения перемножать.                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        int startValue = -5;
+        int stopValue = 5;
+        Console.WriteLine("Старт шага = " + startValue);
+        Console.WriteLine("Конец шага = " + stopValue);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Произведение ряда равно " + ds.Calculate(startValue, stopValue));
+    }
+}

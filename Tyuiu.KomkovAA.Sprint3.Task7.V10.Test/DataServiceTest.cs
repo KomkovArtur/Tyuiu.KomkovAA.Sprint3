1ada57cfa02dfeed37eccada3fd31e088a728eb1
артур комков <stenplay33@gmail.com>
2025-10-30 23:13:02 +0500

    Task7

---
+++ Tyuiu.KomkovAA.Sprint3.Task7.V10.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.KomkovAA.Sprint3.Task7.V10;
+using Tyuiu.KomkovAA.Sprint3.Task7.V10.Lib;
+namespace Tyuiu.KomkovAA.Sprint3.Task7.V10.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            var var = 1.83;
+            Assert.AreEqual(var, ds.GetMassFunction(0, 1)[0]);
+        }
+    }
+}
---
+++ Tyuiu.KomkovAA.Sprint3.Task7.V10/Program.cs
@@ -0,0 +1,43 @@
+﻿using Tyuiu.KomkovAA.Sprint3.Task7.V10;
+using Tyuiu.KomkovAA.Sprint3.Task7.V10.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #3 | Выполнил: Комков А. А. | РППб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Тема: Обработка целочисленной информации                                *");
+        Console.WriteLine("* Задание #7                                                              *");
+        Console.WriteLine("* Вариант #11                                                             *");
+        Console.WriteLine("* Выполнил: Комков Артур Александрович | РППб-25-1                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int startValue = -5;
+        int stopValue = 5;
+        Console.WriteLine("Старт шага = " + startValue);
+        Console.WriteLine("Конец шага = " + stopValue);
+
+        int len = ds.GetMassFunction(startValue, stopValue).Length;
+        double[] valueArray = new double[len];
+        valueArray = ds.GetMassFunction(startValue, stopValue);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("+----------+----------+");
+        Console.WriteLine("+    X     +   f(x)   +");
+        Console.WriteLine("+----------+----------+");
+        for (int i = 0; i <= len - 1; i++)
+        {
+            Console.WriteLine($"{startValue + i}           {valueArray[i]}  ");
+            startValue++;
+        }
+        Console.WriteLine("+----------+----------+");
+    }
+}
